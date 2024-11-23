dfcfed23a6eae0b07d3fa157d71c45aab0abce5c
AlexMilionis <milionisalexis@gmail.com>
2024-11-23 17:56:47 +0200

    Added square size 2048x2048 as max size for zero padding.

--- data/constants.py
+++ data/constants.py
@@ -6,5 +6,4 @@ class Constants():
     DST_PATH     = "D:/repos/MScThesis/github_repo/data/processed"
     IMAGE_DIR = os.path.join(DST_PATH, "AllImages")
     MASK_DIR = os.path.join(DST_PATH, "AllMasks")
-    INPUT_IMAGE_MAX_SIZE = (1080, 1920)
-    # VISUALIZATION_DATA_PATH = os.path.join("D:/repos/MScThesis/github_repo/notebooks/visualization_data",)
+    INPUT_IMAGE_MAX_SIZE = (2048, 2048)#(1080
