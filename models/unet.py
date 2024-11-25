5265ae465e6496a54c70578c7464fb85ec3dc2a3
AlexMilionis <milionisalexis@gmail.com>
2024-11-25 17:49:25 +0200

    Created UNet with pretrained imagenet weights, using resnet encoder.

---
+++ models/unet.py
@@ -0,0 +1,52 @@
+#%%
+import torch
+import segmentation_models_pytorch as smp
+import torch.nn as nn
+import torch.optim as optim
+#%%
+# from segmentation_models_pytorch.encoders import get_encoder_names
+# encoders = get_encoder_names()
+# print(encoders)
+
+#%%
+class UNet(nn.Module):
+    """
+    A wrapper class for a ResNet-based U-Net segmentation model using `segmentation_models_pytorch`.
+
+    Args:
+        encoder_name (str): Name of the encoder (default: "resnet18").
+        encoder_weights (str): Pretrained weights for the encoder (default: "imagenet").
+        in_channels (int): Number of input channels (default: 3 for RGB).
+        classes (int): Number of output classes (default: 2 for binary segmentation).
+    """
+    def __init__(self, encoder_name="resnet18", encoder_weights="imagenet", in_channels=3, classes=2):
+        super(UNet, self).__init__()
+        self.encoder_name = encoder_name
+        self.encoder_weights = encoder_weights
+        self.in_channels = in_channels
+        self.classes = classes
+
+        # Build the U-Net model
+        self.model = self._build_model()
+
+    def _build_model(self):
+        # Use the segmentation_models_pytorch library to build the U-Net
+        return smp.Unet(
+            encoder_name=self.encoder_name,
+            encoder_weights=self.encoder_weights,
+            in_channels=self.in_channels,
+            classes=self.classes,
+        )
+
+    def forward(self, x):
+        # Forward pass
+        return self.model(x)
+
+    def summary(self):
+        # Log a summary of the model architecture
+        print(self.model)
+
+
+# if __name__ == "__main__":
+#     model = UNet(encoder_name="resnet18", encoder_weights="imagenet")
+#     model.summary()
--- scripts/train.py
+++ scripts/train.py
@@ -1,2 +1,28 @@
-from torch.utils.data import random_split
+import torch
+from data.scripts.dataloader import DataLoading
+from torch import optim
+from scripts.hyperparameters import Hyperparameters
+from models.unet import UNet
 
+def train_model():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")\
+    train_loader = DataLoading(mode="train").get_loader()
+    model = UNet(encoder_name="resnet18", encoder_weights="imagenet")
+    model = model.to(device)
+    criterion = None
+    optimizer = optim.Adam(model.parameters(), lr=Hyperparameters.LEARNING_RATE)
+
+    num_epochs = 10 #Hyperparameters.EPOCHS
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0
+        for images, masks, _ in train_loader:
+            images, masks = images.to(device), masks.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, masks)
+            loss.backward()
+            optimizer.step()
+            total_loss = loss.item()
+
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/len(train_loader)}")
