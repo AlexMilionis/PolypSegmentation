326789fc1cbfcb8f91acb95f7de39b477ec489d7
AlexMilionis <milionisalexis@gmail.com>
2024-11-28 13:06:54 +0200

    Implemented function to check amount of positive/negatives samples.

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 from torch.utils.data import Subset
 from data.scripts.dataloader import DataLoading
 from data.constants import Constants
+from hyperparameters import Hyperparameters
 import os
+import torch
 
 def test_for_overlaps_in_datasets():
 
@@ -29,7 +31,6 @@ def test_for_overlaps_in_datasets():
     else:
         print(f"Overlap between train and test sets: {overlap}")
 
-
 def count_images():
     singleframe_images_count, seqframe_images_count = 0,0
     singleframe_masks_count, seqframe_masks_count   = 0,0
@@ -46,6 +47,22 @@ def count_images():
             singleframe_masks_count += 1
     print(f"Singleframe masks count: {singleframe_images_count}, Sequence masks count: {seqframe_images_count}")
 
+def class_balance(mode):
+    loader = DataLoading(mode=mode, include_data='both', shuffle=False).get_loader()
+
+    positive_count, negative_count = 0, 0
+    for img, mask, _ in loader:  # Loop through the DataLoader
+        # Loop through each sample in the batch
+        for single_mask in mask:
+            if torch.sum(single_mask == 1).item():  # Check for 1's (white pixels) in the mask
+                positive_count += 1
+            else:
+                negative_count += 1
+        # print(mask.shape, img.shape)
+    print(f'{mode.capitalize()} mode:')
+    print(f"Positive count: {positive_count}")
+    print(f"Negative count: {negative_count}")
+
+
 if __name__ == '__main__':
-    # test_for_overlaps_in_datasets()
-    count_images()
+    class_balance("test")
