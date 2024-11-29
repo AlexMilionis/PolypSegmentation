baca76b8b07ecfb20084db4eead6554498183b7a
AlexMilionis <milionisalexis@gmail.com>
2024-11-29 15:42:05 +0200

    Create checkpoint for the model, and save it for evaluation.

--- main.py
+++ main.py
@@ -7,22 +7,27 @@ from data.constants import Constants
 from data.scripts.visualization import *
 from data.scripts.dataloader import DataLoading
 from scripts.train import train_model
+from scripts.evaluate import evaluate_model
 from torch.utils.data import Subset
 from scripts.seed import set_seed
 
 if __name__ == '__main__':
     set_seed()
 
-    train_loader = DataLoading(mode="train", include_data = 'both', shuffle=False).get_loader()
-    test_loader = DataLoading(mode="test", include_data = 'both',shuffle=False).get_loader()
+    train_loader = DataLoading(mode="train", include_data = 'single_frames', shuffle=False).get_loader()
+    test_loader = DataLoading(mode="test", include_data = 'single_frames',shuffle=False).get_loader()
+    # # Visualize a single batch by index
+    # visualize_samples_from_random_batch(train_loader, num_samples = 3)
 
-    #
+
     # first_batch = next(iter(train_loader))
     # images, masks, paths = first_batch
     # print(images.shape, masks.shape)
 
-    train_model(train_loader)
+    # train_model(train_loader)
+    model_checkpoint_path = "D:/repos/MScThesis/github_repo/models/model_checkpoint.pth"
+    evaluate_model(test_loader, model_checkpoint_path, visualize_results=True)
 
-    # # Visualize a single batch by index
-    # visualize_samples_from_random_batch(train_loader, num_samples = 3)
+
+
 
