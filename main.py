ca83353981e995b8ed5dce17d40d60a3fc461a27
AlexMilionis <milionisalexis@gmail.com>
2024-12-02 10:28:43 +0200

    Pretrained model saved and given as argument for evaluation script.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scripts.visualization_utils import visualize_data
 from data.scripts.dataloader import DataLoading
-from scripts.train import train_model
+from scripts.transfer_learning import train_model
 from scripts.evaluate import evaluate_model
 from scripts.seed import set_seed
 
@@ -10,16 +10,11 @@ if __name__ == '__main__':
     train_loader = DataLoading(mode="train", include_data = 'single_frames', shuffle=False).get_loader()
     test_loader = DataLoading(mode="test", include_data = 'single_frames',shuffle=False).get_loader()
 
-    visualize_data(test_loader, num_samples = 3)    # Visualize some data
+    # visualize_data(test_loader, num_samples = 3)    # Visualize some data
 
 
-    # first_batch = next(iter(train_loader))
-    # images, masks, paths = first_batch
-    # print(images.shape, masks.shape)
-
-    # train_model(train_loader)
-    # model_checkpoint_path = "D:/repos/MScThesis/github_repo/models/model_checkpoint.pth"
-    # evaluate_model(test_loader, model_checkpoint_path, visualize_results=True)
+    model_checkpoint_path = train_model(train_loader)
+    evaluate_model(test_loader, model_checkpoint_path, visualize_results=True)
 
 
 
