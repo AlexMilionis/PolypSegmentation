22f60ba9fe1217b4855d533ebd6e36eebda86cbd
AlexMilionis <milionisalexis@gmail.com>
2024-12-02 10:26:26 +0200

    Added model checkpoint directory.

--- constants.py
+++ constants.py
@@ -19,11 +19,12 @@ import os
 
 
 class Constants():
-    ABS_PATH     = "D:/repos/MScThesis/github_repo/data"
-    DATASET_PATH = os.path.join(ABS_PATH, "raw")
-    DST_PATH     = "D:/repos/MScThesis/github_repo/data/processed"
+    PROJECT_PATH = "D:/repos/MScThesis/github_repo/"
+    ABS_PATH     = os.path.join(PROJECT_PATH, "data/")
+    DATASET_PATH = os.path.join(ABS_PATH, "raw/")
+    DST_PATH     = os.path.join(ABS_PATH, "processed/")
     IMAGE_DIR = os.path.join(DST_PATH, "AllImages")
     MASK_DIR = os.path.join(DST_PATH, "AllMasks")
     IMAGENET_COLOR_MEANS = [0.485, 0.456, 0.406]
     IMAGENET_COLOR_STDS = [0.229, 0.224, 0.225]
-    MODEL_CHECKPOINT_DIR = "D:/repos/MScThesis/github_repo/models"
+    MODEL_CHECKPOINT_DIR = os.path.join(PROJECT_PATH, "models/")
