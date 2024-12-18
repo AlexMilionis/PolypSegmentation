ca2a214068833eafa5ea999e3f6fba1b822a5760
AlexMilionis <milionisalexis@gmail.com>
2024-12-18 13:28:34 +0200

    Testing new log feature.

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from data.scripts.dataloader import DataLoading
 from scripts.transfer_learning import Trainer
 from scripts.evaluate import Evaluator
 from scripts.seed import set_seed
-import time
 
 set_seed()
-
 if __name__ == '__main__':
 
-    train_loader, val_loader, test_loader = DataLoading(include_data = 'single_frames', shuffle=True).get_loaders()
+    train_loader, val_loader, test_loader = DataLoading(include_data = 'single_frames', shuffle=False).get_loaders()
     # visualize_inputs(train_loader, num_samples = 5)    # Visualize some data
-    Trainer(train_loader, transfer_learning=True).train()
-    Evaluator(test_loader, visualize_predictions=True).evaluate()
+    Trainer(train_loader, val_loader, transfer_learning=True).train()
+    Evaluator(test_loader, visualize_predictions=False).evaluate()
