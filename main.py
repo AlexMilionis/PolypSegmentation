97d88163dbb5ef60ec0ee3a0741015b4bb81226e
AlexMilionis <milionisalexis@gmail.com>
2024-12-18 18:06:10 +0200

    Refactoring.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from src.data.dataloader import DataLoading
 from src.experiments.run_scripts.transfer_learning import Trainer
 from src.scripts.evaluate import Evaluator
 from src.config.seed import set_seed
+from src.scripts.visualization_utils import visualize_inputs
 
 set_seed()
 if __name__ == '__main__':
 
-    train_loader, val_loader, test_loader = DataLoading(include_data = 'single_frames', shuffle=False).get_loaders()
-    # visualize_inputs(train_loader, num_samples = 5)    # Visualize some data
+    train_loader, val_loader, test_loader = DataLoading(include_data = 'single_frames', shuffle=True).get_loaders()
+    visualize_inputs(train_loader, num_samples = 5)    # Visualize some data
     Trainer(train_loader, val_loader, transfer_learning=True).train()
-    Evaluator(test_loader, visualize_predictions=False).evaluate()
+    Evaluator(test_loader, visualize_predictions=True).evaluate()
