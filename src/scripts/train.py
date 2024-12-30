694e094d94a816845a5b91861c6284e2ceb7aef3
AlexMilionis <milionisalexis@gmail.com>
2024-12-30 16:08:38 +0200

    Script under construction.

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,8 +1,8 @@
 import torch
 from tqdm import tqdm
 from src.scripts.metrics import Metrics
-from src.scripts.utils import ExperimentLogger
-from src.experiments.exp1.run_unet import ExperimentImplementation
+from src.scripts.train_utils import ExperimentLogger
+from src.experiments.exp1.run_experiment import ExperimentImplementation
 from torch.cuda.amp import autocast, GradScaler
 
 
@@ -12,20 +12,7 @@ class Trainer(ExperimentImplementation):
 
 
     def _train_one_epoch(self):
-        self.model.train()
-        total_loss = 0
-        for images, masks, _ in self.train_loader:
-            images, masks = images.to(self.device), masks.to(self.device)
-            self.optimizer.zero_grad()
-            #   Forward pass with mixed precision
-            with autocast():
-                outputs = model(images)
-                loss = criterion(outputs, masks)
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-            total_loss += loss.item()
-        return total_loss / len(train_loader)
+        pass
 
     def _validate_one_epoch(self):
         pass
