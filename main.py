0a454719654dd904da7239c4945e1df3fd9405dd
AlexMilionis <milionisalexis@gmail.com>
2024-12-31 18:01:45 +0200

    Added run main.py from cmd functionality, using line: <python main.py config.yaml>

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from src.config.seed import set_seed
 from src.scripts.visualization_utils import visualize_inputs
 from src.experiments.run_experiment import ExperimentImplementation
 from src.scripts.evaluate import Evaluator
+from src.scripts.experiment_utils import ExperimentLogger
+import sys
 
 set_seed()
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        config_name = sys.argv[1]
 
+    config = ExperimentLogger.load_config(config_name)
     train_loader, val_loader, test_loader = DataLoading(include_data = 'single_frames', shuffle=True).get_loaders()
     visualize_inputs(train_loader, num_samples = 5)
     ExperimentImplementation(train_loader, val_loader).train()
