bacd27f562941c363f5c91ef0b79ab2d27b60e7b
AlexMilionis <milionisalexis@gmail.com>
2025-01-02 00:25:57 +0200

    Added new todos.

--- src/scripts/metrics.py
+++ src/scripts/metrics.py
@@ -7,7 +7,6 @@ class Metrics():
         self.false_positives = 0
         self.true_negatives  = 0
 
-
     def add_batch(self, batch_predictions, batch_ground_truths):
         predictions = (batch_predictions > 0.5).float()
         ground_truths = batch_ground_truths.float()
@@ -16,7 +15,6 @@ class Metrics():
         self.false_negatives += torch.sum((predictions == 0) & (ground_truths == 1)).item()
         self.false_positives += torch.sum((predictions == 1) & (ground_truths == 0)).item()
         self.true_negatives  += torch.sum((predictions == 0) & (ground_truths == 0)).item()
-
 
     def recall(self):
         return self.true_positives / (self.true_positives + self.false_negatives)
@@ -36,9 +34,11 @@ class Metrics():
     def jaccard_index(self):
         return self.true_positives / (self.true_positives + self.false_positives + self.false_negatives)
 
-
-    def compute_metrics(self, total_train_loss, len_train_loader, total_val_loss, len_val_loader):
-        return {
+    def compute_metrics(self, test_mode, total_train_loss, len_train_loader, total_val_loss, len_val_loader):
+        metrics = {}
+        if test_mode:
+            metrics['Test Loss'] = None
+        metrics = {
             "Training Loss": total_train_loss / len_train_loader,
             "Validation Loss": total_val_loss / len_val_loader,
             "Recall": self.recall(),
@@ -47,3 +47,6 @@ class Metrics():
             "Dice Score": self.dice_score(),
             "Jaccard Index": self.jaccard_index()
         }
+        return metrics
+
+
