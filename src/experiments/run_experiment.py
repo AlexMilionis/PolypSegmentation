0ffdf55ba4b984b3f08d34ccef0eafbfb037872e
AlexMilionis <milionisalexis@gmail.com>
2025-01-01 21:53:35 +0200

    Criterion, epochs and optimizer are now received from configuration, instead of hyperparameters.py

--- src/experiments/run_experiment.py
+++ src/experiments/run_experiment.py
@@ -1,38 +1,44 @@
 import torch
-from torch import optim
 from src.config.hyperparameters import Hyperparameters
 from src.models.unet import UNet
 from src.models.model_utils import ModelCheckpoint
 import warnings
 from tqdm import tqdm
 from torch.cuda.amp import autocast, GradScaler
-from src.scripts.metrics import Metrics
 from src.scripts.experiment_utils import ExperimentLogger
-
+from src.scripts.metrics import Metrics
+from torch import nn, optim
+from torch.nn import BCEWithLogitsLoss
 
 warnings.filterwarnings('ignore')
 
 
-class ExperimentImplementation:
-    def __init__(self, train_loader, val_loader):
+class Experiment:
+    def __init__(self, train_loader, val_loader, config):
+        self.config = config
         self.train_loader = train_loader
         self.val_loader = val_loader
+
+        self.scaler = GradScaler()  # mixed precision training
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.logger = None
 
         self.model = self._load_model()
-        self.criterion = Hyperparameters.LOSS_FUNCTIONS['binary_crossentropy_with_logits']
-        self.optimizer = optim.Adam(self.model.parameters(), lr=Hyperparameters.LEARNING_RATE)
-        self.scaler = GradScaler()  #   mixed precision training
-        self.num_epochs = Hyperparameters.EPOCHS
-        self.logger = None
+
+        self.num_epochs = config['training']['epochs']
+        self.criterion = getattr(nn, self.config['training']['criterion'])()
+        optimizer_type = getattr(optim, self.config['training']['optimizer']['type'])
+        self.optimizer = optimizer_type(self.model.parameters(),
+                                        lr=self.config['training']['optimizer']['learning_rate'],
+                                        )
 
 
     def _load_model(self):
-        model = UNet(transfer_learning=True).to(self.device)
+        model = UNet(self.config).to(self.device)
         return model
 
 
-    def _train_loop(self):
+    def _train_one_epoch(self):
         self.model.train()
         total_loss = 0
         for images, masks, _ in self.train_loader:
@@ -49,7 +55,7 @@ class ExperimentImplementation:
         return total_loss / len(self.train_loader)
 
 
-    def _validation_loop(self):
+    def _validate_one_epoch(self):
         self.model.eval()
         total_val_loss = 0
         val_metrics = Metrics()
@@ -70,8 +76,10 @@ class ExperimentImplementation:
         with tqdm(range(self.num_epochs), desc="Training Epochs") as pbar:
             for epoch in pbar:
                 torch.cuda.empty_cache()  # Clear GPU memory
-                total_train_loss = self._train_loop()
-                total_val_loss, val_metrics = self._validation_loop()
+                total_train_loss = self._train_one_epoch()
+                # total_train_loss = Trainer.train_one_epoch(self)
+                total_val_loss, val_metrics = self._validate_one_epoch()
+                # total_val_loss, val_metrics = Trainer.validate_one_epoch(self)
                 val_metrics_dict = val_metrics.compute_metrics(total_train_loss, len(self.train_loader), total_val_loss, len(self.val_loader))
                 if epoch==0:
                     self.logger = ExperimentLogger(experiment_name=self.model.name, metrics=val_metrics_dict)
