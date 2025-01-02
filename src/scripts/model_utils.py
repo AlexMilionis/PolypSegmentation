cad4ae60d249414a1fd96ea03f57124a2b371c1d
AlexMilionis <milionisalexis@gmail.com>
2025-01-02 14:45:05 +0200

    save and load checkpoint take directory from config file.

--- src/scripts/model_utils.py
+++ src/scripts/model_utils.py
@@ -6,24 +6,15 @@ import importlib
 class ModelManager:
 
     @staticmethod
-    def save_checkpoint(model, experiment_results_dir):
-        """
-        Saves the model's state dictionary to a checkpoint file.
-        """
-        #   TODO: fix next line
-        checkpoint_path = os.path.join(experiment_results_dir, "checkpoint.pth")
-        # if os.path.exists(checkpoint_path):
-        #     os.remove(checkpoint_path)
+    def save_checkpoint(model, config):
+        checkpoint_path = os.path.join(config['results_dir'], config['experiment_name'], "checkpoint.pth")
         torch.save(model.state_dict(), checkpoint_path)
         return checkpoint_path
 
     @staticmethod
-    def load_checkpoint(model, experiment_results_dir):
-        """
-        Loads the model's state dictionary from a checkpoint file.
-        """
+    def load_checkpoint(model, config):
         #   TODO: fix next line
-        checkpoint_path = os.path.join(experiment_results_dir, "checkpoint.pth")
+        checkpoint_path = os.path.join(config['results_dir'], config['experiment_name'], "checkpoint.pth")
         try:
             model.load_state_dict(torch.load(checkpoint_path))
         except FileNotFoundError:
