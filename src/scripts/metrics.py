e7fa2aa77129905e9446d2054cbce9ed4bf6d260
AlexMilionis <milionisalexis@gmail.com>
2025-01-02 13:19:18 +0200

    Only metrics named in config file are now computed.

--- src/scripts/metrics.py
+++ src/scripts/metrics.py
@@ -16,37 +16,38 @@ class Metrics():
         self.false_positives += torch.sum((predictions == 1) & (ground_truths == 0)).item()
         self.true_negatives  += torch.sum((predictions == 0) & (ground_truths == 0)).item()
 
-    def recall(self):
+    def _recall(self):
         return self.true_positives / (self.true_positives + self.false_negatives)
 
-    def precision(self):
+    def _precision(self):
         return self.true_positives / (self.true_positives + self.false_positives)
 
-    def specificity(self):
+    def _specificity(self):
         return self.true_negatives / (self.true_negatives + self.false_positives)
 
-    def f1(self):
-        return 2*(self.precision() * self.recall())/(self.precision() + self.recall())
+    def _f1_score(self):
+        return 2*(self._precision() * self._recall())/(self._precision() + self._recall())
 
-    def dice_score(self):
+    def _dice_score(self):
         return 2*self.true_positives / (2*self.true_positives + self.false_positives + self.false_negatives)
 
-    def jaccard_index(self):
+    def _jaccard_index(self):
         return self.true_positives / (self.true_positives + self.false_positives + self.false_negatives)
 
-    def compute_metrics(self, test_mode, total_train_loss, len_train_loader, total_val_loss, len_val_loader):
+    def compute_metrics(self, test_mode, **kwargs):
+        metrics_config = kwargs.get("config_metrics", [])
         metrics = {}
         if test_mode:
-            metrics['Test Loss'] = None
-        metrics = {
-            "Training Loss": total_train_loss / len_train_loader,
-            "Validation Loss": total_val_loss / len_val_loader,
-            "Recall": self.recall(),
-            "Precision": self.precision(),
-            "Specificity": self.specificity(),
-            "Dice Score": self.dice_score(),
-            "Jaccard Index": self.jaccard_index()
-        }
+            metrics['TestLoss'] = kwargs.get("test_loss", 0) / kwargs.get("len_test_loader", 1)
+        else:
+            metrics["TrainLoss"] = kwargs.get("train_loss", 0) / kwargs.get("len_train_loader", 1)
+            metrics["ValLoss"] = kwargs.get("val_loss", 0) / kwargs.get("len_val_loader", 1)
+
+        for metric_name in metrics_config:
+            metric_name = '_'+ metric_name
+            if hasattr(self,  metric_name):
+                metric_method = getattr(self, metric_name)
+                metrics[metric_name[1:].title().replace('_','')] = metric_method()
         return metrics
 
 
