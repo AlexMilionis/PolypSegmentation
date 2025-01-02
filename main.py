cdfd68cd37c0cadcb057b090289e50635ed3b498
AlexMilionis <milionisalexis@gmail.com>
2025-01-02 13:17:55 +0200

    Unnecessary python file.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.data.dataloader import DataLoading
 from src.config.seed import set_seed
 from src.scripts.visualization_utils import visualize_inputs
 from src.experiments.run_experiment import Experiment
-from src.scripts.evaluate import Evaluator
 from src.scripts.experiment_utils import ExperimentLogger
 import sys
 
@@ -13,6 +12,8 @@ if __name__ == '__main__':
 
     config = ExperimentLogger.load_config(config_name)
     train_loader, val_loader, test_loader = DataLoading(config).get_loaders()
-    visualize_inputs(train_loader, num_samples = 3)
-    Experiment(train_loader, val_loader, config).execute_training()
-    # Evaluator(test_loader, visualize_predictions=True).evaluate()
+    # visualize_inputs(train_loader, num_samples = 3)
+    exp = Experiment(train_loader, val_loader, test_loader, config)
+    # exp.execute_training()
+    exp.execute_evaluation()
+
