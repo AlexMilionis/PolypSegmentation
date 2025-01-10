fb93594f79b89d17b8e3e2a03840aceed3144156
AlexMilionis <milionisalexis@gmail.com>
2025-01-10 15:23:18 +0200

    The dataset is instantiated using only the split txt file, so split_datasets method is now unnecessary. Moreover, there is no need to create 2 separate whole datasets, with separate transformations, and then subset them into train, val and test.

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import DataLoader, Subset
-from src.data.polyp_dataset import PolypDataset
+from src.data.dataset import PolypDataset
 from src.config.hyperparameters import Hyperparameters
 from src.config.seed import set_generator
 import numpy as np
@@ -9,36 +9,22 @@ import numpy as np
 class DataLoading:
     def __init__(self, config, shuffle_train_data=True):
         # set_seed()
-        self.include_data = config['dataset']['include_data']
-        self.train_ratio = config['dataset']['train_ratio']
-        self.val_ratio = config['dataset']['val_ratio']
-        self.batch_size = config['batch_size']
+        self.config = config
+        self.include_data = self.config['dataset']['include_data']
+        self.train_ratio = self.config['dataset']['train_ratio']
+        self.val_ratio = self.config['dataset']['val_ratio']
+        self.batch_size = self.config['batch_size']
         self.shuffle_train_data = shuffle_train_data  # after we iterate over all batches the data is shuffled
-        self.num_workers = config['num_workers']
+        self.num_workers = self.config['num_workers']
         # self.worker_init_fn = worker_init_fn
-        self.pin_memory = config['pin_memory']
-        self.persistent_workers = config['persistent_workers']
-        #   Load datasets
-        self.dataset_full_with_train_transformations = PolypDataset(config, mode="train", include_data=self.include_data)
-        self.dataset_full_with_val_test_transformations = PolypDataset(config, mode="val_test", include_data=self.include_data)
-
-
-    def split_datasets(self):
-        # Shuffle indices
-        train_indices = torch.randperm(len(self.dataset_full_with_train_transformations), generator=set_generator())
-        # Calculate sizes
-        train_size = int(np.floor(self.train_ratio * len(self.dataset_full_with_train_transformations)))
-        val_size = int(np.floor(self.val_ratio * len(self.dataset_full_with_val_test_transformations)))
-
-        # Split datasets
-        train_dataset = Subset(self.dataset_full_with_train_transformations, train_indices[:train_size])
-        val_dataset = Subset(self.dataset_full_with_val_test_transformations, train_indices[train_size:train_size + val_size])
-        test_dataset = Subset(self.dataset_full_with_val_test_transformations, train_indices[train_size + val_size:])
-        return train_dataset, val_dataset, test_dataset
+        self.pin_memory = self.config['pin_memory']
+        self.persistent_workers = self.config['persistent_workers']
 
 
     def get_loaders(self):
-        train_dataset, val_dataset, test_dataset = self.split_datasets()
+        train_dataset = PolypDataset(self.config, mode="train", include_data=self.include_data)
+        val_dataset = PolypDataset(self.config, mode="val", include_data=self.include_data)
+        test_dataset = PolypDataset(self.config, mode="test", include_data=self.include_data)
         train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=self.shuffle_train_data, num_workers=self.num_workers, pin_memory=self.pin_memory)
         val_loader   = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory)
         test_loader  = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=self.pin_memory)
