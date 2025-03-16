f839ea9e7272d63a1dd63dbad1faee1d573ef17e
AlexMilionis <milionisalexis@gmail.com>
2025-03-16 17:27:06 +0200

    Implemented monai unet, with correct parameterization.

--- scripts/models/monai_unet.py
+++ scripts/models/monai_unet.py
@@ -4,20 +4,27 @@ import torch.nn as nn
 class UNet(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.in_channels = config['model']['input_channels']
-        self.classes = config['model']['num_classes']
+        self.spatial_dims = config['model']['spatial_dims']
+        self.in_channels = config['model']['in_channels']
+        self.classes = config['model']['out_channels']
+        self.channels = config['model']['channels']
+        self.strides = config['model']['strides']
+        self.num_res_units = config['model']['num_res_units']
+        self.norm = config['model']['norm']
+        self.activation = config['model']['act']
         self.model = self._build_model()
+
 
     def _build_model(self):
         return monai_nets.UNet(
-            spatial_dims=2,  # <-- 2D mode
+            spatial_dims=self.spatial_dims,  # <-- 2D mode
             in_channels=self.in_channels,
             out_channels=self.classes,
-            channels=(16, 32, 64, 128, 256),  # Example channel progression
-            strides=(2, 2, 2, 2),  # Downsampling steps
-            num_res_units=2,  # Residual blocks per stage
-            norm="BATCH",  # BatchNorm by default
-            act="RELU",  # Activation function
+            channels=self.channels,  # Example channel progression
+            strides=self.strides,  # Downsampling steps
+            num_res_units=self.num_res_units,  # Residual blocks per stage
+            norm=self.norm,  # BatchNorm by default
+            act=self.activation,  # Activation function
         )
 
     def forward(self, x):
