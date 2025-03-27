e3871f9c4d548b35d8825fea05850d7ab2d99154
AlexMilionis <milionisalexis@gmail.com>
2025-03-27 20:39:06 +0200

    deleted comments

--- scripts/experiments/run_experiment.py
+++ scripts/experiments/run_experiment.py
@@ -62,10 +62,6 @@ class Experiment:
                 val_loss, metrics = self.trainer.validate_one_epoch(self.val_loader, metrics)
                 metrics.compute_metrics(epoch = epoch+1, train_loss = train_loss, val_loss = val_loss)
 
-                # # Print learning rate
-                # current_lr = self.optimizer.param_groups[0]['lr']
-                # print(f"Learning Rate: {current_lr:.6f}")
-
                 # Step the scheduler
                 if isinstance(self.scheduler, lr_scheduler.ReduceLROnPlateau):
                     self.scheduler.step(val_loss)
