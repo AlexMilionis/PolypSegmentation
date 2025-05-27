b13aaac6222765acb408ea1534dab91f399dbbf2
AlexMilionis <milionisalexis@gmail.com>
2025-05-27 22:40:23 +0300

    creates data transformations from scratch

--- scripts/data/transforms.py
+++ scripts/data/transforms.py
@@ -1,147 +1,132 @@
-from torchvision.transforms import v2 as T
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from scripts.constants import Constants
+import numpy as np
+import cv2
 
 
-class Transforms():
-
-    @staticmethod
-    def identity_transform(x):
-        return x
+class Transforms:
 
     @staticmethod
-    def binarize_mask(mask):
-        return (mask > 128).float()
-
+    def train_transforms():
+        return A.Compose(
+            [
+            A.Resize(*Constants.image_size, interpolation=cv2.INTER_LANCZOS4),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.ColorJitter(
+                p=0.5, brightness=(0.6, 1.6), contrast=0.2, saturation=0.1, hue=0.01
+            ),
+            A.Affine(
+                p=0.5,
+                scale=(0.5, 1.5),
+                translate_percent=0.125,
+                rotate=90,
+                interpolation=cv2.INTER_LANCZOS4,
+            ),
+            A.ElasticTransform(p=0.5, interpolation=cv2.INTER_LANCZOS4),
+            A.Normalize(
+                mean=Constants.DATASET_MEANS,
+                std=Constants.DATASET_STDS,
+                max_pixel_value=255,
+            ),
+            ToTensorV2(),
+        ],
+        seed=42
+        )
+    
     @staticmethod
-    def convert_to_float(image):
-        return image.float()
-
+    def val_transforms():
+        return A.Compose([
+            A.Resize(*Constants.image_size, interpolation=cv2.INTER_LANCZOS4),
+            A.Normalize(
+                mean=Constants.DATASET_MEANS,
+                std=Constants.DATASET_STDS,
+                max_pixel_value=255,
+            ),
+            ToTensorV2(),
+        ],
+        seed=42
+        ) 
+    
     @staticmethod
-    def convert_to_01_range(image):
-        return image / 255.0
+    def test_transforms():
+        return A.Compose([
+            A.Resize(*Constants.image_size, interpolation=cv2.INTER_LANCZOS4),
+            A.Normalize(
+                mean=Constants.DATASET_MEANS,
+                std=Constants.DATASET_STDS,
+                max_pixel_value=255,
+            ),
+            ToTensorV2(),
+        ],
+        seed=42
+        )
+    
+# class Transforms:
 
-    @staticmethod
-    def image_and_mask_train_transforms():
-        return T.Compose([
-            T.RandomResizedCrop(size=(512, 512), scale=(0.5, 2)),
-            T.RandomHorizontalFlip(p=0.2),
-            T.RandomVerticalFlip(p=0.2),
-            T.RandomRotation(degrees=15, interpolation=T.InterpolationMode.BILINEAR),
-            T.ElasticTransform(alpha=25, sigma=6, interpolation=T.InterpolationMode.BILINEAR, fill=0)
-        ])
-
-    @staticmethod
-    def image_and_mask_val_test_transforms():
-        return T.Compose([
-            T.Resize(size=(512, 512)),
-        ])
-
-    @staticmethod
-    def image_train_transforms():
-
-        return T.Compose([
-            T.Lambda(Transforms.convert_to_float),
-            T.Lambda(Transforms.convert_to_01_range),
-            T.ColorJitter(brightness=0.25, contrast=0.25, hue=0.25),
-            T.RandomGrayscale(p=0.1),  # convert the image to grayscale
-            # T.GaussianBlur(kernel_size=(3, 3)), # blur the image with a 3x3 kernel
-            T.RandomApply([T.GaussianBlur(3)], p=0.2),
-            T.Normalize(mean=Constants.TRAIN_DATA_MEANS, std=Constants.TRAIN_DATA_STDS),
-        ])
-
-    @staticmethod
-    def image_val_test_transforms():
-        return T.Compose([
-            T.Lambda(Transforms.convert_to_01_range),
-            T.Normalize(mean=Constants.TRAIN_DATA_MEANS, std=Constants.TRAIN_DATA_STDS),
-        ])
-
-    @staticmethod
-    def mask_train_transforms():
-        return T.Compose([
-            T.Lambda(Transforms.binarize_mask),
-        ])
-
-    @staticmethod
-    def mask_val_test_transforms():
-        return T.Compose([
-            T.Lambda(Transforms.binarize_mask),
-        ])
-
-
-# # scripts/data/transforms.py
-# from torchvision.transforms import v2 as T, InterpolationMode
-# import torch
-#
-# # TODO: understand this, or better use Albumentations
-# class Transforms_new:
 #     @staticmethod
-#     def train_dict_transforms():
-#         """
-#         Return a Compose that expects a dict:
-#         {
-#           "input": (C,H,W) image tensor,
-#           "mask":  (1,H,W) mask tensor
-#         }
-#         and returns them with random augmentations applied in sync.
-#         """
-#         return T.Compose([
-#             # RandomResizedCrop applies to both "input" & "mask" by default
-#             T.RandomResizedCrop(size=(512, 512),
-#                                 scale=(0.5, 1.5),
-#                                 interpolation=InterpolationMode.BILINEAR),
-#             T.RandomHorizontalFlip(prob=0.2),  # flips both input & mask
-#             T.RandomVerticalFlip(prob=0.2),
-#             T.RandomRotation(degrees=15, interpolation=InterpolationMode.BILINEAR),
-#             # If you want an elastic transform that handles the mask too:
-#             # T.ElasticTransform(...)
-#             # etc.
-#             T.ElasticTransform(alpha=25, sigma=6, interpolation=T.InterpolationMode.BILINEAR, fill=0)
+#     def image_and_mask_train_transforms():
+
+#         return A.Compose([
+#             # ============= Spatial Transformations =============
+#             # Random crop/scale with area preservation
+#             A.RandomResizedCrop(size=(512,512), 
+#                                 scale=(0.5, 1.0), 
+#                                 interpolation=cv2.INTER_LINEAR, 
+#                                 mask_interpolation=cv2.INTER_NEAREST, 
+#                                 p=1),
+#             # Aggressive flips & rotations
+#             A.HorizontalFlip(p=0.5),
+#             A.VerticalFlip(p=0.5),
+#             A.ShiftScaleRotate(
+#                 shift_limit=0.1, 
+#                 scale_limit=0.2, 
+#                 rotate_limit=25, 
+#                 p=0.5),
+#             A.GridDistortion(
+#                 num_steps=5,
+#                 distort_limit=0.3,
+#                 p=0.4
+#             ),
+#             # A.ElasticTransform(alpha=25, sigma=6, p=0.3),
+
+#             # ============= Intensity Transformations =============
+#             A.RandomBrightnessContrast(
+#                 brightness_limit=0.15, 
+#                 contrast_limit=0.15, 
+#                 p=0.5),
+#             A.GaussianBlur(blur_limit=(3, 7), p=0.3),
+
+            
+#             # Color / intensity transforms on image only
+#             # A.ColorJitter(brightness=0.1, contrast=0.1, hue=0.1, p=0.2),
+#             # A.RandomGamma(gamma_limit=(80, 120), p=0.2),
+
+#             # A.CoarseDropout(
+#             #     num_holes_range=(2, 8),       # Replaces min_holes/max_holes
+#             #     hole_height_range=(32, 32),   # Fixed size (32px)
+#             #     hole_width_range=(32, 32),    # Fixed size (32px)
+#             #     fill=0,                       # Same as fill_value=0
+#             #     fill_mask=0,                  # Replaces mask_fill_value
+#             #     p=0.4
+#             # ),
+#             A.Normalize(
+#                 mean=Constants.TRAIN_DATA_MEANS, 
+#                 std=Constants.TRAIN_DATA_STDS, 
+#                 max_pixel_value=1),
+#             ToTensorV2(transpose_mask=True), # [height, width, num_channels] -> [num_channels, height, width]
 #         ])
-#
+
 #     @staticmethod
-#     def val_dict_transforms():
-#         """
-#         Deterministic resizing for val/test.
-#         """
-#         return T.Compose([
-#             T.Resize(size=(512, 512), interpolation=InterpolationMode.BILINEAR),
+#     def image_and_mask_val_test_transforms():
+
+#         return A.Compose([
+#             A.Resize(
+#                 height=512, 
+#                 width=512, 
+#                 interpolation=cv2.INTER_LINEAR, 
+#                 mask_interpolation=cv2.INTER_NEAREST),
+#             A.Normalize(mean=Constants.TRAIN_DATA_MEANS, std=Constants.TRAIN_DATA_STDS, max_pixel_value=1),
+#             ToTensorV2(transpose_mask=True),    # [height, width, num_channels] -> [num_channels, height, width]
 #         ])
-#
-#     @staticmethod
-#     def image_extra_transform(mode):
-#         """
-#         Optionally add color, normalization, or other image-only transforms
-#         AFTER the dictionary transform.
-#         We'll just do a few examples here.
-#         """
-#         from torchvision.transforms import functional as F
-#
-#         def _image_only_transform(img_tensor: torch.Tensor) -> torch.Tensor:
-#             # Convert to float [0..1]
-#             img_tensor = img_tensor.float() / 255.0
-#             # Example: color jitter only if train
-#             if mode == "train":
-#                 # manually do random color ops, or do a T.ColorJitter on single image
-#                 # e.g. T.ColorJitter(0.2,0.2,0.2,0.1)(img_tensor)
-#                 pass
-#             # e.g. normalization
-#             # means = [0.485, 0.456, 0.406]
-#             # stds  = [0.229, 0.224, 0.225]
-#             # ...
-#             return img_tensor
-#
-#         return _image_only_transform  # a function that takes a tensor
-#
-#     @staticmethod
-#     def mask_extra_transform(mode):
-#         """
-#         For the mask, maybe binarize it or convert to float, etc.
-#         """
-#         from torchvision.transforms import functional as F
-#
-#         def _mask_only_transform(msk_tensor: torch.Tensor) -> torch.Tensor:
-#             # Already shape (1,H,W), typical mask usage
-#             # maybe we want to binarize:
-#             return (msk_tensor > 0).float()
-#         return _mask_only_transform
