b2486730eaa70377c46a608a88ba30135806f4ee
AlexMilionis <milionisalexis@gmail.com>
2025-05-27 22:43:18 +0300

    overfit test is now functionality of the dataloader

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-from scripts.data.dataloader_new import DataLoading
+from scripts.data.dataloader import DataLoading
 from scripts.seed import set_seed
 from scripts.experiments.run_experiment import Experiment
 from scripts.experiments.experiment_utils import ExperimentLogger
 from scripts.visualizations.visualization_utils import visualize_data
-from scripts.data.get_train_mean_std import get_train_mean_std
-import sys, yaml
 import warnings
 warnings.filterwarnings("ignore")
-
 
 set_seed()
 if __name__ == '__main__':
 
     config = ExperimentLogger.parse_arguments()
 
-    train_loader, val_loader, test_loader = DataLoading(config).get_dataloaders(run_in_colab=True)
-    # train_loader, val_loader, test_loader = DataLoadingCloud(config).get_loaders(viz=True)
-
-    visualize_data(config, train_loader, num_samples=5)
+    dl = DataLoading(config, mode="overfit")
+    dl.build_loaders()
     
-    exp = Experiment(train_loader, val_loader, test_loader, config)
+    # # loop through the train_loader
+    # for batch in dl.train_loader:
+    #     print(f"---batch---")
+    
+    visualize_data(config, dl.train_loader, num_samples=5)
+    
+    exp = Experiment(dl.train_loader, dl.val_loader, dl.test_loader, config)
 
     metrics = exp.execute_training()
 
