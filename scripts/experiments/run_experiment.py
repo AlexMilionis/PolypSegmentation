c6742fb91937d92648ed8c632da68c73935a2132
AlexMilionis <milionisalexis@gmail.com>
2025-07-04 16:56:42 +0300

    transfered trainer_engine logic inside run_experiment

--- scripts/experiments/run_experiment.py
+++ scripts/experiments/run_experiment.py
@@ -3,15 +3,18 @@ import numpy as np
 from scripts.models.model_utils import ModelManager
 from tqdm import tqdm
 # from torch.cuda.amp import GradScaler
-from torch.amp import GradScaler
+from torch.amp import GradScaler, autocast
 from scripts.experiments.experiment_utils import ExperimentLogger
-from torch import nn, optim
-from torch.optim import lr_scheduler
 from scripts.experiments.trainer_engine import Trainer, EarlyStopping, Optimizer
 # from scripts.experiments.metrics import Metrics
 from scripts.models.loss import Dice_CE_Loss
 
 from scripts.visualizations.visualization_utils import plot_loss_curves
+from monai import metrics as mm
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from scripts.visualizations.visualization_utils import visualize_outputs
+
 
 
 class Experiment:
@@ -27,9 +30,27 @@ class Experiment:
         self.num_epochs = config['epochs']
         self.criterion = Dice_CE_Loss(self.config)
 
-        self.opt_object = Optimizer(self.config, self.model)
+        self.optimizer = AdamW(
+            self.model.parameters(), 
+            lr=self.config['learning_rate'], 
+            weight_decay=self.config['weight_decay'],
+            )
+        
+        self.scheduler = CosineAnnealingLR(
+            self.optimizer, 
+            T_max=self.num_epochs, 
+            eta_min=1e-5,
+        )
         
         self.trainer = Trainer(self.config, self.model, self.opt_object.optimizer, self.criterion, self.scaler, self.device)
+        self.mean_dice = mm.DiceMetric(include_background=False, reduction="mean")
+        self.mean_iou = mm.MeanIoU(include_background=False, reduction="mean")
+        self.precision = mm.ConfusionMatrixMetric(include_background=False, metric_name="precision")
+        self.recall = mm.ConfusionMatrixMetric(include_background=False, metric_name="sensitivity")
+        # self.get_f1_score = mm.ConfusionMatrixMetric(include_background=False, metric_name="f1_score")
+        self.accuracy = mm.ConfusionMatrixMetric(include_background=False, metric_name="accuracy")
+
+
         self.metrics = []
 
     def execute_training(self, load_checkpoint=False):
@@ -38,11 +59,31 @@ class Experiment:
             # self.metrics = Metrics(self.device)
 
             for epoch in pbar:                    
-                train_loss = self.trainer.train_one_epoch(self.train_loader)
+                # train_loss = self.trainer.train_one_epoch(self.train_loader)
 
+                # Train Loop
+                self.model.train()
+                total_train_loss = 0
+                for images, masks, _ in self.train_loader:
+                    images = images.to(self.device)
+                    masks  = masks.to(self.device)
+                    self.optimizer.zero_grad()
+                    # Mixed precision forward pass
+                    with autocast('cuda'):
+                        outputs = self.model(images)
+                        # print(f"Output shape: {outputs.shape}, Mask shape: {masks.shape}")
+                        # print(f"Outputs: {outputs}, Masks: {masks}")
+                        batch_loss = self.criterion(outputs, masks) 
+                        
+                    self.scaler.scale(batch_loss).backward()
+                    self.scaler.step(self.optimizer)
+                    self.scaler.update()
+                    total_train_loss += batch_loss
+
+                # Update metrics on train epoch end
                 self.metrics.append({
                     'epoch': epoch+1,
-                    'train_loss': train_loss,
+                    'train_loss': total_train_loss/len(self.train_loader),
                     'val_loss': np.nan,
                     'meanIoU': np.nan,
                     'meanDice': np.nan,
@@ -50,12 +91,43 @@ class Experiment:
                     'recall': np.nan,
                     'accuracy': np.nan,
                 })
-                val_loss, self.metrics = self.trainer.validate_one_epoch(self.val_loader, self.metrics)
 
-                # self.metrics.compute_metrics(epoch = epoch+1, train_loss = train_loss, val_loss = val_loss)
+                # Validation Loop
+                self.model.eval()
+                total_val_loss = 0
+                threshold = 0.5
+                with torch.no_grad():
+                    for images, masks, _ in self.val_loader:
+                        images = images.to(self.device)
+                        masks = masks.to(self.device)
+                        with autocast('cuda'):
+                            outputs = self.model(images)
+                            batch_loss = self.criterion(outputs, masks)
+                        probs = torch.sigmoid(outputs)
+                        preds = (probs > threshold).float()
+                        total_val_loss += batch_loss
 
-                # # Save model checkpoint
-                # ModelManager.save_model_checkpoint(self.model, self.config, self.metrics, epoch)
+                        # Update metrics for all batches
+                        self.mean_iou(y_pred=preds, y=masks)
+                        self.mean_dice(y_pred=preds, y=masks)
+                        self.precision(y_pred=preds, y=masks)
+                        self.recall(y_pred=preds, y=masks)
+                        self.accuracy(y_pred=preds, y=masks)
+
+                    # Update metrics on validation epoch end
+                    self.metrics[-1]["val_loss"] = total_val_loss/len(self.val_loader)
+                    self.metrics[-1]["meanIoU"] = self.mean_iou.aggregate()
+                    self.metrics[-1]["meanDice"] = self.mean_dice.aggregate()
+                    self.metrics[-1]["precision"] = self.precision.aggregate()
+                    self.metrics[-1]["recall"] = self.recall.aggregate()
+                    self.metrics[-1]["accuracy"] = self.accuracy.aggregate()
+
+                    # Reset metrics after aggregation
+                    self.mean_iou.reset()
+                    self.mean_dice.reset()
+                    self.precision.reset()
+                    self.recall.reset()
+                    self.accuracy.reset()
 
                 # # Early stopping check
                 # early_stopping.check_early_stop(val_loss)
@@ -63,7 +135,7 @@ class Experiment:
                 #     print(f"Early stopping at epoch {epoch}")
                 #     break
 
-                self.opt_object.scheduler_step(val_loss)
+                self.scheduler.step()
 
         # save final model
         ModelManager.save_checkpoint(self.model, self.config)
@@ -71,7 +143,57 @@ class Experiment:
 
     def execute_evaluation(self):
 
-        test_loss, self.metrics = self.trainer.validate_one_epoch(self.test_loader, self.metrics, test=True)
+        # test_loss, self.metrics = self.trainer.validate_one_epoch(self.test_loader, self.metrics, test=True)
+        self.model.eval()
+        total_test_loss = 0
+        threshold = 0.5
+        already_visualized = False
+        with torch.no_grad():
+            for images, masks, paths in self.test_loader:
+                images = images.to(self.device)
+                masks = masks.to(self.device)
+                with autocast('cuda'):
+                    outputs = self.model(images)
+                    batch_loss = self.criterion(outputs, masks)
+                probs = torch.sigmoid(outputs)
+                preds = (probs > threshold).float()
+                total_test_loss += batch_loss
+
+                # Update metrics for all batches
+                self.mean_iou(y_pred=preds, y=masks)
+                self.mean_dice(y_pred=preds, y=masks)
+                self.precision(y_pred=preds, y=masks)
+                self.recall(y_pred=preds, y=masks)
+                self.accuracy(y_pred=preds, y=masks)
+
+                if not already_visualized:
+                    visualize_outputs(self.config, images, masks, preds, paths)
+                    already_visualized = True
+
+            # Compute final metrics after processing all batches
+            test_loss = total_test_loss/len(self.test_loader)
+            
+            # Only append one test entry after processing all batches
+            self.metrics.append({
+                "epoch": -1,
+                "train_loss": np.nan,
+                "val_loss": test_loss,
+                "meanIoU": self.mean_iou.aggregate(),
+                "meanDice": self.mean_dice.aggregate(),
+                "precision": self.precision.aggregate(),
+                "recall": self.recall.aggregate(),
+                "accuracy": self.accuracy.aggregate()
+                })
+
+
+            # Reset metrics after aggregation
+            self.mean_iou.reset()
+            self.mean_dice.reset()
+            self.precision.reset()
+            self.recall.reset()
+            self.accuracy.reset()
+
+
         # metrics.compute_metrics(test_loss = test_loss, mode="test")
         ExperimentLogger.log_metrics(self.config, self.metrics)
         plot_loss_curves(self.config)
--- scripts/experiments/trainer_engine.py
+++ scripts/experiments/trainer_engine.py
@@ -6,103 +6,103 @@ from torch import optim
 from torch.optim import lr_scheduler
 from monai import metrics as mm
 
-class Trainer:
-    def __init__(self, config, model, optimizer, criterion, scaler, device):
-        self.config = config
-        self.model = model
-        self.optimizer = optimizer
-        self.criterion = criterion
-        self.scaler = scaler
-        self.device = device
+# class Trainer:
+#     def __init__(self, config, model, optimizer, criterion, scaler, device):
+#         self.config = config
+#         self.model = model
+#         self.optimizer = optimizer
+#         self.criterion = criterion
+#         self.scaler = scaler
+#         self.device = device
 
-        self.mean_dice = mm.DiceMetric(include_background=False, reduction="mean")
-        self.mean_iou = mm.MeanIoU(include_background=False, reduction="mean")
-        self.precision = mm.ConfusionMatrixMetric(include_background=False, metric_name="precision")
-        self.recall = mm.ConfusionMatrixMetric(include_background=False, metric_name="sensitivity")
-        # self.get_f1_score = mm.ConfusionMatrixMetric(include_background=False, metric_name="f1_score")
-        self.accuracy = mm.ConfusionMatrixMetric(include_background=False, metric_name="accuracy")
+#         self.mean_dice = mm.DiceMetric(include_background=False, reduction="mean")
+#         self.mean_iou = mm.MeanIoU(include_background=False, reduction="mean")
+#         self.precision = mm.ConfusionMatrixMetric(include_background=False, metric_name="precision")
+#         self.recall = mm.ConfusionMatrixMetric(include_background=False, metric_name="sensitivity")
+#         # self.get_f1_score = mm.ConfusionMatrixMetric(include_background=False, metric_name="f1_score")
+#         self.accuracy = mm.ConfusionMatrixMetric(include_background=False, metric_name="accuracy")
 
 
-    def train_one_epoch(self, train_loader):
-        self.model.train()
-        total_loss = 0
-        for images, masks, _ in train_loader:
-            images = images.to(self.device)
-            masks  = masks.to(self.device)
-            self.optimizer.zero_grad()
-            # Mixed precision forward pass
-            with autocast('cuda'):
-                outputs = self.model(images)
-                # print(f"Output shape: {outputs.shape}, Mask shape: {masks.shape}")
-                # print(f"Outputs: {outputs}, Masks: {masks}")
-                loss = self.criterion(outputs, masks) 
+#     def train_one_epoch(self, train_loader):
+#         self.model.train()
+#         total_loss = 0
+#         for images, masks, _ in train_loader:
+#             images = images.to(self.device)
+#             masks  = masks.to(self.device)
+#             self.optimizer.zero_grad()
+#             # Mixed precision forward pass
+#             with autocast('cuda'):
+#                 outputs = self.model(images)
+#                 # print(f"Output shape: {outputs.shape}, Mask shape: {masks.shape}")
+#                 # print(f"Outputs: {outputs}, Masks: {masks}")
+#                 loss = self.criterion(outputs, masks) 
                 
-            self.scaler.scale(loss).backward()
-            self.scaler.step(self.optimizer)
-            self.scaler.update()
-            total_loss += loss
-            # print(f"Loss: {loss}")
+#             self.scaler.scale(loss).backward()
+#             self.scaler.step(self.optimizer)
+#             self.scaler.update()
+#             total_loss += loss
+#             # print(f"Loss: {loss}")
 
-        return total_loss / len(train_loader)
+#         return total_loss / len(train_loader)
 
-    def validate_one_epoch(self, loader, metrics, test=False):
-        self.model.eval()
-        total_loss = 0
-        already_visualized = False
-        threshold = 0.5
-        with torch.no_grad():
-            for images, masks, paths in loader:
-                images = images.to(self.device)
-                masks = masks.to(self.device)
-                with autocast('cuda'):
-                    outputs = self.model(images)
-                    loss = self.criterion(outputs, masks)
-                probs = torch.sigmoid(outputs)
-                preds = (probs > threshold).float()
-                total_loss += loss
+#     def validate_one_epoch(self, loader, metrics, test=False):
+#         self.model.eval()
+#         total_loss = 0
+#         already_visualized = False
+#         threshold = 0.5
+#         with torch.no_grad():
+#             for images, masks, paths in loader:
+#                 images = images.to(self.device)
+#                 masks = masks.to(self.device)
+#                 with autocast('cuda'):
+#                     outputs = self.model(images)
+#                     loss = self.criterion(outputs, masks)
+#                 probs = torch.sigmoid(outputs)
+#                 preds = (probs > threshold).float()
+#                 total_loss += loss
 
-                # Update metrics for all batches
-                self.mean_iou(y_pred=preds, y=masks)
-                self.mean_dice(y_pred=preds, y=masks)
-                self.precision(y_pred=preds, y=masks)
-                self.recall(y_pred=preds, y=masks)
-                self.accuracy(y_pred=preds, y=masks)
+#                 # Update metrics for all batches
+#                 self.mean_iou(y_pred=preds, y=masks)
+#                 self.mean_dice(y_pred=preds, y=masks)
+#                 self.precision(y_pred=preds, y=masks)
+#                 self.recall(y_pred=preds, y=masks)
+#                 self.accuracy(y_pred=preds, y=masks)
 
-                if test and not already_visualized:
-                    visualize_outputs(self.config, images, masks, preds, paths)
-                    already_visualized = True
+#                 if test and not already_visualized:
+#                     visualize_outputs(self.config, images, masks, preds, paths)
+#                     already_visualized = True
 
-            # Compute final metrics after processing all batches
-            computed_loss = total_loss/len(loader)
+#             # Compute final metrics after processing all batches
+#             computed_loss = total_loss/len(loader)
             
-            if test:
-                # Only append one test entry after processing all batches
-                metrics.append({
-                    "epoch": -1,
-                    "train_loss": np.nan,
-                    "val_loss": computed_loss,
-                    "meanIoU": self.mean_iou.aggregate(),
-                    "meanDice": self.mean_dice.aggregate(),
-                    "precision": self.precision.aggregate(),
-                    "recall": self.recall.aggregate(),
-                    "accuracy": self.accuracy.aggregate()
-                })
-            else:   # validation
-                metrics[-1]["val_loss"] = computed_loss
-                metrics[-1]["meanIoU"] = self.mean_iou.aggregate()
-                metrics[-1]["meanDice"] = self.mean_dice.aggregate()
-                metrics[-1]["precision"] = self.precision.aggregate()
-                metrics[-1]["recall"] = self.recall.aggregate()
-                metrics[-1]["accuracy"] = self.accuracy.aggregate()
+#             if test:
+#                 # Only append one test entry after processing all batches
+#                 metrics.append({
+#                     "epoch": -1,
+#                     "train_loss": np.nan,
+#                     "val_loss": computed_loss,
+#                     "meanIoU": self.mean_iou.aggregate(),
+#                     "meanDice": self.mean_dice.aggregate(),
+#                     "precision": self.precision.aggregate(),
+#                     "recall": self.recall.aggregate(),
+#                     "accuracy": self.accuracy.aggregate()
+#                 })
+#             else:   # validation
+#                 metrics[-1]["val_loss"] = computed_loss
+#                 metrics[-1]["meanIoU"] = self.mean_iou.aggregate()
+#                 metrics[-1]["meanDice"] = self.mean_dice.aggregate()
+#                 metrics[-1]["precision"] = self.precision.aggregate()
+#                 metrics[-1]["recall"] = self.recall.aggregate()
+#                 metrics[-1]["accuracy"] = self.accuracy.aggregate()
 
-            # Reset metrics after aggregation
-            self.mean_iou.reset()
-            self.mean_dice.reset()
-            self.precision.reset()
-            self.recall.reset()
-            self.accuracy.reset()
+#             # Reset metrics after aggregation
+#             self.mean_iou.reset()
+#             self.mean_dice.reset()
+#             self.precision.reset()
+#             self.recall.reset()
+#             self.accuracy.reset()
 
-        return computed_loss, metrics
+#         return computed_loss, metrics
     
 
 class EarlyStopping:
@@ -125,58 +125,58 @@ class EarlyStopping:
 
 
 
-class Optimizer:
-    def __init__(self, config, model):
-        self.config = config
-        self.model = model
-        optimizer_type = getattr(optim, self.config['optimizer'])
-        self.optimizer = optimizer_type(
-            self.model.parameters(), 
-            lr=float(self.config['learning_rate']), 
-            weight_decay=self.config['weight_decay']
-            )
+# class Optimizer:
+#     def __init__(self, config, model):
+#         self.config = config
+#         self.model = model
+#         optimizer_type = getattr(optim, self.config['optimizer'])
+#         self.optimizer = optimizer_type(
+#             self.model.parameters(), 
+#             lr=float(self.config['learning_rate']), 
+#             weight_decay=self.config['weight_decay']
+#             )
         
 
-        if self.config["scheduler"] == "CosineAnnealingLR":
-            self.scheduler = lr_scheduler.CosineAnnealingLR(
-                optimizer=self.optimizer,
-                T_max=self.config["epochs"],
-                eta_min=1e-5,
-            )
+#         if self.config["scheduler"] == "CosineAnnealingLR":
+#             self.scheduler = lr_scheduler.CosineAnnealingLR(
+#                 optimizer=self.optimizer,
+#                 T_max=self.config["epochs"],
+#                 eta_min=1e-5,
+#             )
 
-        elif self.config["scheduler"] == "ReduceLROnPlateau":
-            self.scheduler = lr_scheduler.ReduceLROnPlateau(
-                optimizer=self.optimizer,
-                mode="min",
-                patience=5,
-                factor=0.5,
-                verbose=True,
-                # min_lr=1e-6
-            )
+#         elif self.config["scheduler"] == "ReduceLROnPlateau":
+#             self.scheduler = lr_scheduler.ReduceLROnPlateau(
+#                 optimizer=self.optimizer,
+#                 mode="min",
+#                 patience=5,
+#                 factor=0.5,
+#                 verbose=True,
+#                 # min_lr=1e-6
+#             )
 
-        elif self.config["scheduler"] == "CosineAnnealingWarmRestarts":
-            self.scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
-                optimizer=self.optimizer,
-                T_0=10,  # Number of iterations for the first restart
-                T_mult=2,  # Factor by which to increase T_0 after each restart
-                eta_min=1e-6,  # Minimum learning rate
-            )
+#         elif self.config["scheduler"] == "CosineAnnealingWarmRestarts":
+#             self.scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
+#                 optimizer=self.optimizer,
+#                 T_0=10,  # Number of iterations for the first restart
+#                 T_mult=2,  # Factor by which to increase T_0 after each restart
+#                 eta_min=1e-6,  # Minimum learning rate
+#             )
 
 
-        elif self.config["scheduler"] in [None, "None"]:
-            self.scheduler = None
+#         elif self.config["scheduler"] in [None, "None"]:
+#             self.scheduler = None
 
-        else:
-            raise ValueError(f"Unknown scheduler: {self.config['scheduler']}") 
+#         else:
+#             raise ValueError(f"Unknown scheduler: {self.config['scheduler']}") 
         
 
-    def scheduler_step(self, val_loss):
-        if isinstance(self.scheduler, lr_scheduler.ReduceLROnPlateau):
-            old_lr = self.optimizer.param_groups[0]['lr']
-            self.scheduler.step(val_loss)
-            new_lr = self.optimizer.param_groups[0]['lr']
-            if new_lr < old_lr:
-                print(f"Learning rate reduced from {old_lr:.6f} to {new_lr:.6f}")
-        elif isinstance(self.scheduler, lr_scheduler.CosineAnnealingLR):
-            self.scheduler.step()
+#     def scheduler_step(self, val_loss):
+#         if isinstance(self.scheduler, lr_scheduler.ReduceLROnPlateau):
+#             old_lr = self.optimizer.param_groups[0]['lr']
+#             self.scheduler.step(val_loss)
+#             new_lr = self.optimizer.param_groups[0]['lr']
+#             if new_lr < old_lr:
+#                 print(f"Learning rate reduced from {old_lr:.6f} to {new_lr:.6f}")
+#         elif isinstance(self.scheduler, lr_scheduler.CosineAnnealingLR):
+#             self.scheduler.step()
 
